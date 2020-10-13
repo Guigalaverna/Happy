842d42cca66f4476e8996998e89b24cc973a6387
Guigalaverna <guigalaverna@gmail.com>
2020-10-13 13:37:48 -0300

    :rocket: Refatoração do Server

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,23 +1,11 @@
 import express from 'express'
-
-import { getRepository } from 'typeorm'
+import routes from './routes'
 
 import './database/connection'
 
 const app = express()
 
 app.use(express.json())
-
-app.post('/orphanages', (req, res) => {
-  const {
-    name,
-    latitude,
-    longitude,
-    about,
-    instructions,
-    opening_hours,
-    open_on_weekends,
-  } = req.body
-})
+app.use(routes)
 
 app.listen(3333, () => console.log('Server running'))
