82cd98764cdcd829955bc4b5a9c0044c6bbb2e6b
Guigalaverna <guigalaverna@gmail.com>
2020-10-13 12:58:51 -0300

    :rocket: Server Atualizado

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,13 +1,23 @@
 import express from 'express'
 
+import { getRepository } from 'typeorm'
+
 import './database/connection'
 
 const app = express()
 
 app.use(express.json())
 
-app.get('/', (req, res) => {
-  return res.json({ message: "hello" })
+app.post('/orphanages', (req, res) => {
+  const {
+    name,
+    latitude,
+    longitude,
+    about,
+    instructions,
+    opening_hours,
+    open_on_weekends,
+  } = req.body
 })
 
 app.listen(3333, () => console.log('Server running'))
