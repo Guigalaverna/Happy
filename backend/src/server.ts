5ff050f6f700aa99dc8d2f659ad9975f739ac9e5
Guigalaverna <guigalaverna@gmail.com>
2020-10-15 17:55:21 -0300

    :rocket: Uso do CORS

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,10 +1,13 @@
 import express from 'express'
 import path from 'path'
 import routes from './routes'
+import cors from 'cors'
 
 import './database/connection'
 
 const app = express()
+
+app.use(cors())
 
 app.use(express.json())
 app.use(routes)
