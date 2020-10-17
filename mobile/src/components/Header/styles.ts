2c43e4017b0b6d886c46d065895d79e49270c09c
Guilherme Galaverna <guigalaverna@gmail.com>
2020-10-17 18:44:47 -0300

    :rocket: Componente Header

---
+++ mobile/src/components/Header/styles.ts
@@ -0,0 +1,21 @@
+import { StyleSheet } from 'react-native'
+
+export default StyleSheet.create({
+  container: {
+    padding: 24,
+    backgroundColor: '#f9fafc',
+    borderBottomWidth: 1,
+    borderColor: '#DDE3F0',
+    paddingTop: 50,
+
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center'
+  },
+
+  title: {
+    fontFamily: 'Nunito_600SemiBold',
+    color: '#8fa7b3',
+    fontSize: 14,
+  },
+})
