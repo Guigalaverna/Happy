650ff4bc34c2056858e1546c9b6b7c841707ea98
Guilherme Galaverna <guigalaverna@gmail.com>
2020-10-17 18:47:22 -0300

    :rocket: Arquivo de Rotas

---
+++ mobile/src/routes.tsx
@@ -0,0 +1,62 @@
+import React from 'react';
+
+import { NavigationContainer } from '@react-navigation/native'
+import { createStackNavigator } from '@react-navigation/stack'
+
+const { Navigator, Screen } = createStackNavigator()
+
+import OrphanagesMap from './pages/OrphanagesMap'
+import OrphanageDetails from './pages/OrphanageDetails';
+import OrphanageData from './pages/CreateOrphanage/OrphanageData';
+import SelectMapPosition from './pages/CreateOrphanage/SelectMapPosition';
+
+import Header from './components/Header'
+
+const Routes = () => {
+  return (
+    <NavigationContainer>
+      <Navigator 
+        screenOptions={{ 
+          headerShown: false, 
+          cardStyle: { backgroundColor: '#F2F3F5'} 
+        }}
+        >
+
+        <Screen 
+          component={OrphanagesMap} 
+          name='OrphanagesMap' 
+        />
+
+        <Screen 
+          component={OrphanageDetails} 
+          name='OrphanageDetails' 
+          options={{ 
+            headerShown: true,
+            header: () => <Header showCancel={false} title='Orfanato'/>
+          }}
+        />
+
+        <Screen
+          component={OrphanageData} 
+          name='OrphanageData'
+          options={{ 
+            headerShown: true, 
+            header: () => <Header title='Infome dos dados'/>
+          }} 
+        />
+
+        <Screen 
+          component={SelectMapPosition} 
+          name='SelectMapPosition' 
+          options={{ 
+            headerShown: true, 
+            header: () => <Header title='Selecione no mapa'/>
+          }} 
+        />
+
+      </Navigator>
+    </NavigationContainer>
+  )
+}
+
+export default Routes
