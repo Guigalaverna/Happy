7ebfcd9e1bad307ff83af3ebe2fa62cf36ebdec0
Guilherme Galaverna <guigalaverna@gmail.com>
2021-02-01 23:38:07 -0300

    feat: Novas Rotas

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -1,15 +1,21 @@
-import { Router } from 'express'
-import multer from 'multer'
+import { Router } from "express";
+import multer from "multer";
 
-import uploadConfig from './config/upload'
+import uploadConfig from "./config/upload";
 
-import OrphanagesController from './controllers/OrphanagesController'
+import OrphanagesController from "./controllers/OrphanagesController";
+import UserController from "./controllers/UserController";
+import User from "./models/User";
 
-const routes = Router()
-const upload = multer(uploadConfig)
+const routes = Router();
+const upload = multer(uploadConfig);
 
-routes.get('/orphanages', OrphanagesController.index)
-routes.get('/orphanages/:id', OrphanagesController.show)
-routes.post('/orphanages', upload.array('images'),OrphanagesController.create)
+routes.get("/orphanages", OrphanagesController.index);
+routes.get("/orphanages/:id", OrphanagesController.show);
+routes.post("/orphanages", upload.array("images"), OrphanagesController.create);
 
-export default routes
+routes.get("/users", UserController.index);
+routes.get("/users/:id", UserController.show);
+routes.post("/users", UserController.create);
+
+export default routes;
